692a1d0be8e2c4895ecbd313eb051c70e2572ada
Riley Wilton <30930649+wireboy5@users.noreply.github.com>
2020-11-30 10:00:55 -0500

    Now supports infix to tree. Fixed typo

--- __main__.py
+++ __main__.py
@@ -1,5 +1,5 @@
 import arithmatic_parsing
 
 parser = arithmatic_parsing.Parser()
-a = parser.infix_to_prefix("(testVar+2*3)-(testVar2+3*3)")
+a = parser.infix_to_tree("(testVar+2*3)-(testVar2+3*3)")
 print(a)
